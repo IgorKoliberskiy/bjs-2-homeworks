ef9d461724e0d2f4ff0275be51e80af2a12fa7f3
Игорь Колиберский <kolibry60rus@mail.ru>
2022-06-13 20:50:27 +0300

    Сorrected remarks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,18 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
-  "use strict"
-  arr = [];
   
   let discriminant = Math.pow(b, 2) - 4 * a * c;
 
-  if (discriminant < 0 || a === 0) { //
+  if (a === 0) {
     arr.length = 0;
-  } else if (discriminant === 0) {
-    arr[0] = -b / (2 * a);
-  } else {    
+  } else if (discriminant > 0) {
     arr[0] = (-b + Math.sqrt(discriminant))/(2 * a);
     arr[1] = (-b - Math.sqrt(discriminant))/(2 * a);
+  } else if (discriminant === 0) {    
+    arr[0] = -b / (2 * a);
   }  
     
   return arr; // array
@@ -22,29 +22,34 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
-  "use strict"
 
-  let interestRate = percent / 100 * 1 / 12; // процентная ставка
-  let monthlyPayment; // ежемесячный платеж
-  let numberOfMonths; // количество месяцев
   let now = new Date(); // текущая дата
-  let loanBody = amount - contribution; // тело кредита
-  
-  if (Number.isNaN(percent) || percent < 0) {
-    alert(`Параметр "Процентная ставка" содержит неправильное значение ${"percent"}`);
-  } else if (Number.isNaN(contribution) || contribution < 0) {
-    alert(`Параметр "Начальный взнос" содержит неправильное значение ${"contribution"}`);
-  } else if (Number.isNaN(amount) || amount <= contribution) {
-    alert(`Параметр "Общая стоимость" содержит неправильное значение ${"amount"}`);
-  } else if (now >= date) {
-    alert(`Параметр "Срок ипотеки" содержит неправильное значение ${date}`);
-  }
-  
-  numberOfMonths = (date.getFullYear() - now.getFullYear()) * 12 - date.getMonth() + now.getMonth();
 
-  monthlyPayment = (loanBody * (interestRate + (interestRate / (Math.pow((1 + interestRate), numberOfMonths) - 1)))).toFixed(2);
+  if (isNaN(percent) || percent < 0) {
+    return(`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+  } else if (isNaN(contribution) || contribution < 0) {
+    return(`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+  } else if (isNaN(amount)) {
+    return(`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+  } else if (now >= date) {
+    return(`Параметр "Срок ипотеки" содержит неправильное значение "${date}"`);
+  }
+
+  // процентная ставка для расчета ежемесячного платежа
+  let interestRate = percent / 100 * 1 / 12; 
+  
+  // тело кредита
+  let loanBody = amount - contribution;
+  
+  // количество месяцев
+  let numberOfMonths = (date.getFullYear() - now.getFullYear()) * 12 - date.getMonth() + now.getMonth();
+
+  // ежемесячный платеж
+  let monthlyPayment = loanBody * (interestRate + (interestRate / (Math.pow((1 + interestRate), numberOfMonths) - 1))); 
+  
   console.log(monthlyPayment);
 
+  totalAmount = Math.round(numberOfMonths * monthlyPayment * 100) / 100;
    
   return totalAmount;
 }
