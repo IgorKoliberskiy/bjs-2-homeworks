dfbecfd915c6080759cd5a6b9078549ca4f779aa
Игорь Колиберский <kolibry60rus@mail.ru>
2022-06-15 14:25:47 +0300

    Functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,24 +3,49 @@ function getArrayParams(arr) {
   let min, max, sum, avg;
 
   // Ваш код
-
+  min = 100;
+  max = -100;
+  sum = 0;
+    
+  for (i = 0; i < arr.length; i += 1) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  
+  avg = Number((sum / arr.length).toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
   // Ваш код
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
   // Ваш кода
-  // for ...
+  // for...
+  for (let i = 0; i < arrOfArr.length; i += 1) {
+    func(arrOfArr[i]);
+    
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
   
   return max;
 }
@@ -28,4 +53,19 @@ function makeWork(arrOfArr, func) {
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let min, max, difference;
+  min = Infinity;
+  max = -Infinity;
+  
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return difference = Math.abs(max - min);
 }
