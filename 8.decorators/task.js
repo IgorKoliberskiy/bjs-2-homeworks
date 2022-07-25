6b25e9eda6e7dbe02c4a1ab81df7706abbb60516
Игорь Колиберский <kolibry60rus@mail.ru>
2022-07-25 22:49:02 +0300

    8.decorators - homework

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,50 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = args.join(', '); 
+		let objectInCache = cache.findIndex(item => item.hash === hash);
+	    
+		if (objectInCache !== -1) {
+				console.log("Из кэша: " + cache[objectInCache].result); 
+				return "Из кэша: " + cache[objectInCache].result;
+		} 
+
+		let result = func(...args); 
+		cache.push({hash, result}); 
+		if (cache.length > 5) { 
+			cache.shift(); 
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;  
+		}
+	return wrapper;
 }
 
+function debounceDecoratorNew(func, delay) {
+	let timeout = null;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+	function wrapper (...args) {
+		if (timeout === null) {
+			func(...args);
+	  }
+	  clearTimeout(timeout);
+	  timeout = setTimeout(() => func(...args), delay);
+	}
+	return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func,delay) {
+	let timeout = null;
+	
+	function wrapper (...args){
+		if (timeout === null) {
+			func(...args);
+			clearTimeout(timeout);
+			timeout = setTimeout(() => func(...args), delay)
+		} 
+		wrapper.count++;
+	} 
+	wrapper.count = 0;
+	return wrapper;
 }
